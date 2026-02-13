0e77b3cd2f7810de8b95571f2111dd5c53b46011
salma2002-la <harbsalma18@gmail.com>
2026-02-13 19:31:51 +0300

    Enhance triangle validation in get_triangle_type
    Refactor triangle type function to improve validation.

--- task-1/triangle_func.py
+++ task-1/triangle_func.py
@@ -1,18 +1,35 @@
 class IncorrectTriangleSides(Exception):
+    """Исключение для некорректных сторон треугольника."""
     pass
+
 
 def get_triangle_type(a, b, c):
     """
+    Возвращает тип треугольника:
+    "nonequilateral", "isosceles", "equilateral".
+
+    Примеры (doctest):
+
     >>> get_triangle_type(3, 3, 3)
     'equilateral'
-    >>> get_triangle_type(3, 3, 2)
+    >>> get_triangle_type(3, 3, 5)
     'isosceles'
     >>> get_triangle_type(3, 4, 5)
     'nonequilateral'
+    >>> get_triangle_type(1, 1, 10)
+    Traceback (most recent call last):
+        ...
+    IncorrectTriangleSides
     """
-    sides = sorted([a, b, c])
-    if sides[0] <= 0 or sides[0] + sides[1] <= sides[2]:
-        raise IncorrectTriangleSides("Invalid triangle sides")
+
+    if not all(isinstance(x, (int, float)) for x in (a, b, c)):
+        raise IncorrectTriangleSides
+
+    if a <= 0 or b <= 0 or c <= 0:
+        raise IncorrectTriangleSides
+
+    if a + b <= c or a + c <= b or b + c <= a:
+        raise IncorrectTriangleSides
 
     if a == b == c:
         return "equilateral"
@@ -20,3 +37,8 @@ def get_triangle_type(a, b, c):
         return "isosceles"
     else:
         return "nonequilateral"
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
